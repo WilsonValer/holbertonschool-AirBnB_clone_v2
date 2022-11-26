4191126c55fa3458cb38af7eb5f3b529c782e771
WilsonValer <nederig21@gmail.com>
2022-11-26 10:06:14 -0500

    was created all method in the new module db_storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,11 +1,20 @@
+#!/usr/bin/python3
 """
 module for the DBSstorage
 """
 
-from models.base_model import Base
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, scoped_session
 from os import getenv
+
+from models.amenity import Amenity
+from models.city import City
+from models.state import State
+from models.user import User
+from models.place import Place
+from models.review import Review
+from models.base_model import BaseModel, Base
+
 
 class DBStorage():
     __engine = None
@@ -27,6 +36,7 @@ class DBStorage():
     def all(self, cls=None):
         """ query on the current database session (self.__session) all
             objects depending of the class name (argument cls)
+            if cls = None return all types of objects
         """
         class_dict = {}
         if cls is None:
@@ -39,4 +49,31 @@ class DBStorage():
                 key = "{}.{}".format(type(result).__name__, result.id)
                 class_dict[key] = result
         return class_dict
-                
+
+    def new(self, obj):
+        """add the object to the current database session"""
+        self.__session.commit()
+
+    def save(self):
+        """commit all changes of the current database session"""
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """delete from the current database session"""
+        self.__session.delete(obj)
+        self.save()
+
+    def reload(self):
+        """create all tables in the database
+            sessionmaker is A factory teh produce session
+            scope_session invoque to the sessionmaker
+        """
+        Base.metadata.create_all(self.__engine)
+        Session = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(session_factory)
+        self.__session = Session()
+
+    def close(self):
+        """method to close the session"""
+        self.__session.close()
+
