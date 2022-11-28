95409858fbd835040642236be4440cee0e929e13
JulianZea <cesarjulianzeabravo@gmail.com>
2022-11-28 00:48:11 -0500

    updated file user

--- models/user.py
+++ models/user.py
@@ -23,6 +23,9 @@ class User(BaseModel, Base):
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
 
+        places = relationship("Place", backref="user",
+                              cascade="all, delete, delete-orphan")
+
     else:
         email = ""
         password = ""
