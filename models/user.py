5f75689f7906e65b2744fe5a92e7c87b1232a763
JulianZea <cesarjulianzeabravo@gmail.com>
2022-11-28 00:19:54 -0500

    updated file user

--- models/user.py
+++ models/user.py
@@ -1,11 +1,30 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
-from models.base_model import BaseModel
+"""This is the user class"""
+from models.base_model import Base, BaseModel
+from sqlalchemy import Column, Integer, String
+import sqlalchemy
+from sqlalchemy.orm import backref, relationship
+from os import getenv
 
 
-class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+
+class User(BaseModel, Base):
+    """This is the class for user
+    Attributes:
+        email: email address
+        password: password for you login
+        first_name: first name
+        last_name: last name
+    """
+    __tablename__ = 'users'
+    if getenv("HBNB_TYPE_STORAGE") == 'db':
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
