18db378be3b022d986b6e88037a47d810568c2eb
WilsonValer <nederig21@gmail.com>
2022-11-28 12:03:23 -0500

    was modified the code in the module place

--- models/place.py
+++ models/place.py
@@ -8,11 +8,8 @@ from os import getenv
 
 if getenv("HBNB_TYPE_STORAGE") == "db":
     place_amenity = Table('place_amenity', Base.metadata,
-                          Column('place_id', String(60), ForeignKey("places.id"),
-                                 primary_key=True, nullable=False),
-                          Column('amenity_id', String(60),
-                                 ForeignKey("amenities.id"),
-                                 primary_key=True, nullable=False))
+                          Column('place_id', String(60), ForeignKey("places.id"), nullable=False),
+                          Column('amenity_id', String(60),ForeignKey("amenities.id"), nullable=False))
 
 
 class Place(BaseModel, Base):
@@ -43,7 +40,7 @@ class Place(BaseModel, Base):
         latitude = Column(Float, nullable=True)
         longitude = Column(Float, nullable=True)
 
-        reviews = relationship("Review", backref="Place",
+        reviews = relationship("Review", backref="place",
                                cascade="all, delete, delete-orphan")
 
         amenities = relationship("Amenity", secondary=place_amenity,
