17034e500d0dd860c95be28c1668b1648d5bac7c
WilsonValer <nederig21@gmail.com>
2022-11-30 18:39:54 -0500

    Write a script that starts a Flask web application:

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -41,13 +41,10 @@ def imanumber(n):
     return "{:d} is a number".format(n)
 
 
-@app.route('/number_template/<n>', strict_slashes=False)
+@app.route("/number_template/<int:n>", strict_slashes=False)
 def number_template(n):
-    """H1 tag: Number: n inside the tag BODY"""
-    if not n.isnumeric():
-        abort(404)
-    return render_template('5-number.html', key_name=n)
-
+    """Display the int on a h1 tag"""
+    return (render_template('5-number.html', num=escape(n)))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
