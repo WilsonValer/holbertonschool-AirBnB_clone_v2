9a2d3cb35309ee4aff7325fd3862d1fa2bad79f7
WilsonValer <nederig21@gmail.com>
2022-11-29 23:09:32 -0500

    2-c_route.py

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" Write a script that starts a Flask web application:"""
+from flask import Flask
+
+app = Flask(__name__)
+
+#  Utilice el route()decorador para vincular una función a una URL.
+
+
+@app.route("/")
+def hello_world():
+    """ method that return mesages in the server"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    """ method that return mesages in the server"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def text(text):
+    """Write a script that starts a Flask web application:"""
+    return f'C is {text}'
+
+
+if (__name__ == '__main__'):
+    app.run(debug=True, host="0.0.0.0", port=5000)
