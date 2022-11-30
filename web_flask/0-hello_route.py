a679739c7448b405e32f12070b6796ab5cda1a5c
WilsonValer <nederig21@gmail.com>
2022-11-29 20:09:00 -0500

    was added documentation

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
+""" Write a script that starts a Flask web application:"""
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
+    """ method that return mesages in the server"""
     return "Hello HBNB!"
 
 
