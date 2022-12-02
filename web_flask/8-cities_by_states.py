4258044900673234fb2ee13c84dddb9e5f099a6a
WilsonValer <nederig21@gmail.com>
2022-12-02 15:44:01 -0500

    was correct the error of code

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -7,12 +7,18 @@ from models.state import State
 app = Flask(__name__)
 
 
-
 @app.route("/cities_by_states", strict_slashes=False)
 def cities_by_states():
     """Dispaly the list of cities"""
     cities = storage.all(State).values()
     return (render_template('8-cities_by_states.html', states=cities))
+
+
+@app.teardown_appcontext
+def teardown(self):
+    """to clse the conection"""
+    storage.close()
+
 
 if (__name__ == '__main__'):
     app.run(host="0.0.0.0", port=5000)
