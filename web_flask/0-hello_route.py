785a51ad2345acfdae60a293b43699195f4f5fc9
WilsonValer <nederig21@gmail.com>
2022-12-06 00:58:45 -0500

    task was add

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,11 +5,11 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", strict_slashes=False)
 def hello_world():
     """ method that return mesages in the server"""
     return "Hello HBNB!"
 
 
 if (__name__ == '__main__'):
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=8000)
